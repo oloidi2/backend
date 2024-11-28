ea47c1b1874e5776bd033ce00f72fcc07afb25de
Olumide <olumidet451@gmail.com>
2024-11-28 12:34:04 +0000

    integrating morgan for logger

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const fs = require('fs');
 const { MongoClient } = require('mongodb');
 
